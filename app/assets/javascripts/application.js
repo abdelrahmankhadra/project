4ffaff5ede254ddf5793b2cb4fe3c1d054e3d16f
aboodkhadra <aboodkhadra7@gmail.com>
2017-11-19 14:42:45 +0200

    3.11

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,9 @@
 //= require turbolinks
 //= require jquery3
 //= require_tree .
-// Twitter
 // work section
+// Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
 $(document).ready(function(){
   // Smooth scrolling
