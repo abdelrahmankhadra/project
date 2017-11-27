5f9b2f8a8db5d8d37e14fb3a0e9e885d20d2390c
aboodkhadra <aboodkhadra7@gmail.com>
2017-11-27 10:36:55 +0200

    3.11

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  get 'simple_pages/index'
+  get 'simple_pages/landing_page'
   root 'simple_pages#landing_page'
 
   resources :orders, only: %i[index show create destroy]
