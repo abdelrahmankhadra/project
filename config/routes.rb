0c97c4300ee318f5c151c4bf93b3a6c66cda1256
aboodkhadra <aboodkhadra7@gmail.com>
2017-12-04 11:06:51 +0200

    5.2 Forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  get '/products/:id', to: 'products#show'
   get 'simple_pages/landing_page'
-  root 'products#index'
+  root 'simple_pages#landing_page'
 
   resources :orders, only: %i[index show create destroy]
-  resources :users, only: [:new, :create, :edit, :update, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
