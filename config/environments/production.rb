aecdbae85013898e873235e9be7630ce21f3e687
aboodkhadra <aboodkhadra7@gmail.com>
2017-12-06 14:39:10 +0200

    5.3 sending emails mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,10 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
+ :authentication       => "plain",
+ :enable_starttls_auto => true,
  :user_name            => "aboodkhadra2017@gmail.com",
  :password             => "P@ss123.321W0rd",
- :authentication       => "plain",
-:enable_starttls_auto => true
+ domain:'heroku.com'
 }
 end
