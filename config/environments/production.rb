bf1c332e51ffe9fbc12c077c7ca863edcb39e8de
aboodkhadra <aboodkhadra7@gmail.com>
2017-12-09 10:46:21 +0200

    5.3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,4 @@
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :authentication       => "plain",
- :enable_starttls_auto => true,
- :user_name            => "aboodkhadra2017@gmail.com",
- :password             => "P@ss123.321W0rd",
- domain:'heroku.com'
-}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
