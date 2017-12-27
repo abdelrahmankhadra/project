3d7ce7ff15e77f294052e46499fee64aee115f00
aboodkhadra <aboodkhadra7@gmail.com>
2017-12-27 11:38:18 +0200

    6.2 Unit tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Comment do
+  let (:product) {Product.create!(name:"race bike",description: "bike bike bike",image_url: "product1.png",colour: "green",price: "27")}
+  let (:user) {User.create!(first_name: "mohammed", last_name:"khadra", email: "mkhadra@gmail.com", password: "123456")}
+
   context "Testing validations" do
 
     it "is not valid without a rating" do
@@ -11,6 +14,18 @@ describe Comment do
     end
     it "is not valid without a user id" do
       expect(Comment.new(user_id:nil)).not_to be_valid
+    end
+
+    it "is not vailid without a product id" do
+      expect(Comment.new(product_id:nil)).not_to be_valid
+    end
+
+    it "is not valid if rating is not integer" do
+      expect(Comment.new(rating: "Hello World", user: user, body: "good product", product: product)).not_to be_valid
+    end
+
+    it "is a valid comment" do
+      expect(Comment.new(rating: 4, user: user, body: "very nice product", product: product)).to be_valid
     end
 
   end
