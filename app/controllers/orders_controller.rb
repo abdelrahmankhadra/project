7c75fc3286bc2a128e6ef7933ce568364e2f2afd
aboodkhadra <aboodkhadra7@gmail.com>
2018-01-03 14:00:57 +0200

    Order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,34 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  def index;
+  def index
+    @orders = Order.includes(:product, :user).where(user_id: current_user.id).all
   end
 
-  def show;
+  def show
+    @order = Order.find(params[:id])
   end
 
   def new;
   end
 
   def create;
+    @product = Product.find(params[:product_id])
+    @order = @product.orders.build(product: @product)
+    @order.user = current_user
+    @order.total = @product.price
+    if @order.save
+      redirect_to order_path(@order)
+      flash[:notice] = "Order made successfully!"
+    else
+      flash[:error] = "Something went wrong!"
+    end
   end
 
   def destroy;
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:user_id, :total)
   end
 end
