1112c488bece9e32a09c18ced9425cc4ae2627f0
aboodkhadra <aboodkhadra7@gmail.com>
2018-01-20 16:50:47 +0200

    6.6 Creating Payments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118095326) do
+ActiveRecord::Schema.define(version: 20180120144858) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180118095326) do
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
-    t.float "total"
+    t.decimal "total"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
