43c47f09e799fedbc0823196db26c6b58aa75b06
aboodkhadra <aboodkhadra7@gmail.com>
2018-02-18 15:40:22 +0200

    6.13 setup real-time updates with actioncable for product reviews v2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  port: '587',
+  authentication: :plain,
+  enable_starttls_auto: true,
+  user_name: 'abdelrahmankhadra@gmail.com',
+  password: 'A@123456789',
+  domain: 'heroku.com'
+}
